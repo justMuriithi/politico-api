bef5ed602093cbaf0670e3a9c948ccb43e07acd4
justMuriithi <justMuriithi@github.com>
2019-02-07 00:46:18 +0300

    [Finishes #163746316] Writes test for get all parties

--- app/tests/test_party.py
+++ app/tests/test_party.py
@@ -1,4 +1,4 @@
-from .test_skeleton import Skeleton
+from .skeleton_test import Skeleton
 from app.version1.routes import parties
 
 class TestParty(Skeleton):
@@ -24,4 +24,14 @@ class TestParty(Skeleton):
         self.assertEqual(data['message'], 'Your political party was created successfully')
         self.assertEqual(res.status_code, 201)
 
-    
+    def test_get_parties(self):
+        res = self.client.post('/api/version1/parties', json = self.party)
+        res = self.client.post('/api/version1/parties', json = self.party)
+
+        res = self.client.get('/api/version1/parties')
+        data = res.get_json()
+
+        self.assertEqual(data['status'], 200)
+        self.assertEqual(data['message'], 'Request was successful')
+        self.assertEqual(len(data['data']), 2)
+        self.assertEqual(res.status_code, 200)
