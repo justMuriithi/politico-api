adfebbe13536c894c4ae8cd952e175b5f470c4bf
justMuriithi <justMuriithi@github.com>
2019-02-06 23:45:19 +0300

    [Finishes #163746241] Writes test for create party

--- app/tests/test_party.py
+++ app/tests/test_party.py
@@ -1,4 +1,5 @@
 from .test_skeleton import Skeleton
+from app.version1.routes import parties
 
 class TestParty(Skeleton):
 
@@ -10,6 +11,10 @@ class TestParty(Skeleton):
             "hq_address": "Eldoret",
             "logo_url":"url"
         }
+    # clear all lists after tests
+    def tearDown(self):
+        super().tearDown()
+        parties.clear()
 
     def test_create_party(self):
         res = self.client.post('/api/version1/parties/', json = self.party)
@@ -19,14 +24,4 @@ class TestParty(Skeleton):
         self.assertEqual(data['message'], 'Your political party was created successfully')
         self.assertEqual(res.status_code, 201)
 
-    def test_get_parties(self):
-        res = self.client.post('/api/version1/parties', json = self.party)
-        res = self.client.post('/api/version1/parties', json = self.party)
-
-        res = self.client.get('/api/version1/parties')
-        data = res.get_json()
-
-        self.assertEqual(data['status'], 200)
-        self.assertEqual(data['message'], 'Request was successful')
-        self.assertEqual(len(data['data']), 2)
-        self.assertEqual(res.status_code, 200)
+    
