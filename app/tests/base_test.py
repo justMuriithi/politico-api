3ccbdc2f65480b969a36477a11100382b955de0d
justMuriithi <justMuriithi@github.com>
2019-02-10 18:39:47 +0300

    [Finishes #163847709] Implements more OOP

--- app/tests/base_test.py
+++ app/tests/base_test.py
@@ -1,7 +1,10 @@
 from app import create_app
 import unittest
+from app.version1.models.db import Database
+from app.version1.models.parties_model import Party
+from app.version1.models.offices_model import Office
 
-class Skeleton(unittest.TestCase):
+class Base(unittest.TestCase):
     
     def setUp(self):
         self.app = create_app('testing')
@@ -12,3 +15,7 @@ class Skeleton(unittest.TestCase):
     def tearDown(self):
         self.app = None
         self.item_list.clear()
+        Office.offices.clear()
+        Party.parties.clear()
+
+
